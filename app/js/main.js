0499e80384b5cac8f20b7ad0b48e573aa4684d75
ShariseMo <1009708910@qq.com>
2017-03-18 18:04:22 +0800

    3017-3-18

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,18 @@
-// var oReq = new XMLHttpRequest();
-// oReq.addEventListener('load',function(e){
-//     var arr = JSON.parse(e.currentTarget.responseText);
-//     console.log(arr);
-//     var resultHtml = '<ul>';
-//     for(var i=0; i<arr.length;i++){
-//         var todo = arr[i];
-//         resultHtml += '<li>' + todo.title + ' - ' + (todo.done?'【完成】':'【待处理】') + '</li>';
-//     }
-//     resultHtml += '</ul>';
-//     $("#todoList").html(resultHtml);
-// });
+var oReq = new XMLHttpRequest();
+oReq.addEventListener('load',function(e){
+    var arr = JSON.parse(e.currentTarget.responseText);
+    console.log(arr);
+    var resultHtml = '<ul>';
+    for(var i=0; i<arr.length;i++){
+        var todo = arr[i];
+        resultHtml += '<li>' + todo.title + ' - ' + (todo.done?'【完成】':'【待处理】') + '</li>';
+    }
+    resultHtml += '</ul>';
+    $("#todoList").html(resultHtml);
+});
 
-// oReq.open('GET','/v1/foobizzle.json');
-// oReq.send();
+oReq.open('GET','/v1/foobizzle.json');
+oReq.send();
 
 
 
